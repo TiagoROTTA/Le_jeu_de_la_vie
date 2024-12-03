5f0a7e943b7ee76d8d4f3fe34dac2b806d57eeb5
viacomeb <come.bertin@gmail.com>
2024-12-03 14:04:47 +0100

    cell obstacle public

--- coding/cell.hpp
+++ coding/cell.hpp
@@ -3,8 +3,9 @@
 class Cell {
 private:
     bool living;                    // État de la cellule
-    bool obstacle;                  // La cellule est un obstacle ou non 
+    //bool obstacle;                  // La cellule est un obstacle ou non 
 public:
+    bool obstacle;
     Cell();                                     // Constructeur par défaut
     Cell(bool living, bool obstacle);   // Constructeur avec état initial
     bool getState() const;                      // Retourne l'état de la cellule
